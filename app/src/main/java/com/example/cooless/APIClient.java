0bac5efabb882e44048ccf0e9d07e821e602a26a
Camin McCluskey <camin.mccluskey@skyscanner.net>
2019-10-12 14:27:28 +0200

    Add APIClient class

---
+++ app/src/main/java/com/example/cooless/APIClient.java
@@ -0,0 +1,22 @@
+package com.example.cooless;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+
+    static Retrofit getClient() {
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://reqres.in")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
