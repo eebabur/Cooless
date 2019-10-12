0e56543be6fef9f173b1cfba8f345b1aae4a00c2
Emre Babur <emre.babur@skyscanner.net>
2019-10-12 12:14:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cooless/CoolessApp.kt
@@ -0,0 +1,8 @@
+package com.example.cooless
+
+import android.app.Application
+
+class CoolessApp : Application() {
+
+    var userState: UserState = UserState()
+}
---
+++ app/src/main/java/com/example/cooless/UserState.kt
@@ -0,0 +1,5 @@
+package com.example.cooless
+
+data class UserState(
+    val name: String = "Emre Babur"
+)
