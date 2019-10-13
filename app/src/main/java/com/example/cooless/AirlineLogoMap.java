d7c2a3a5231c36b8bc7b721d9cb3f75f0acc34eb
Camin McCluskey <camin.mccluskey@skyscanner.net>
2019-10-13 09:40:20 +0200

    store map

---
+++ app/src/main/java/com/example/cooless/AirlineLogoMap.java
@@ -0,0 +1,12 @@
+package com.example.cooless;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+
+public class AirlineLogoMap {
+    static HashMap<String, Integer> map;
+
+    public  AirlineLogoMap() {
+
+    }
+}
