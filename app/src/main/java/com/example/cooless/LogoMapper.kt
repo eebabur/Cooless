bf3109bcda425222e195e5ef523acb42694352e7
Camin McCluskey <camin.mccluskey@skyscanner.net>
2019-10-13 13:26:41 +0200

    Add default logo

--- app/src/main/java/com/example/cooless/LogoMapper.kt
+++ app/src/main/java/com/example/cooless/LogoMapper.kt
@@ -7,7 +7,8 @@ object LogoMapper {
         "American Airlines" to R.drawable.ic_american_airlines_logo_2013,
         "Swiss International Air Lines" to R.drawable.ic_swiss_logo,
         "Vueling Airlines" to R.drawable.ic_logo_vueling,
-        "Level Airlines" to R.drawable.ic_level
+        "Level Airlines" to R.drawable.ic_level,
+        "default" to R.drawable.ic_british_airways_logo
     )
 }
 
